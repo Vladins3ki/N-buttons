0f3b63f2baa0689c944ef01ab1e16b99d162fd49
Vladins3ki <123756024+Vladins3ki@users.noreply.github.com>
2023-01-31 16:05:17 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+document.getElementById("reset").onclick = function(){
+    window.location.reload();
+}
+
+ document.getElementById("submit").onclick = function(){
+    let numberValue = document.getElementById("number").value;
+    for( let i = 1; i <= numberValue; ++i) {
+        let button = document.createElement('button')
+        button.className = "button"
+        button.id =  i
+        button.value = "You clicked the wrong button!" 
+        button.addEventListener('click', () => {
+           document.getElementById("format").innerHTML = document.getElementById(i).value;
+       })
+
+       document.body.appendChild(button)
+   }
+   let randomNumber = Math.floor(Math.random() * numberValue) +1;
+   document.getElementById(randomNumber).value = "You clicked the right button!";
+   document.getElementById("number").value = "";
+
+ }
