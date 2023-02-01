4ed76d80a328017b4cf448a87a03f3210338d9f5
Vladins3ki <123756024+Vladins3ki@users.noreply.github.com>
2023-02-01 16:42:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-document.getElementById("reset").onclick = function(){
+ function restart(){
     window.location.reload();
 }
 
- document.getElementById("submit").onclick = function(){
+ function getSubmit(){
     let numberValue = document.getElementById("number").value;
-    for( let i = 1; i <= numberValue; ++i) {
-        let button = document.createElement('button')
-        button.className = "button"
-        button.id =  i
-        button.value = "You clicked the wrong button!" 
-        button.addEventListener('click', () => {
-           document.getElementById("format").innerHTML = document.getElementById(i).value;
+        for( let i = 1; i <= numberValue; ++i) {
+             let button = document.createElement('button')
+             button.className = "button"
+             button.id =  i
+             button.value = "You clicked the wrong button!" 
+             button.addEventListener('click', () => {
+             document.getElementById("format").innerHTML = document.getElementById(i).value;
        })
 
        document.body.appendChild(button)
    }
-   let randomNumber = Math.floor(Math.random() * numberValue) +1;
-   document.getElementById(randomNumber).value = "You clicked the right button!";
-   document.getElementById("number").value = "";
+    let randomNumber = Math.floor(Math.random() * numberValue) +1;
+    document.getElementById(randomNumber).value = "You clicked the right button!";
+    document.getElementById("number").value = "";
 
  }
