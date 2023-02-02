55279d85483617729ec1fabfa0a7c7552da438ae
Vladins3ki <123756024+Vladins3ki@users.noreply.github.com>
2023-02-02 15:58:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,14 @@
              let button = document.createElement('button')
              button.className = "button"
              button.id =  i
-             button.value = "You clicked the wrong button!" 
+             button.value = "You clicked the wrong button!"
              button.addEventListener('click', () => {
              document.getElementById("format").innerHTML = document.getElementById(i).value;
-       })
+             })
 
-       document.body.appendChild(button)
-   }
+            document.body.appendChild(button)
+         }
     let randomNumber = Math.floor(Math.random() * numberValue) +1;
     document.getElementById(randomNumber).value = "You clicked the right button!";
     document.getElementById("number").value = "";
-
  }
